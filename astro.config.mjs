c513db65a2bce0bf0bd3ac7257a4902a5932a53d
francoTrautweiler <francotraut9@gmail.com>
2026-03-01 21:12:13 -0300

    prueba1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  // Reemplaza 'usuario' por tu nombre en GitHub y 'cvtecnico' por el nombre de tu repo
   site: 'https://trautweilerFranco.github.io',
-  base: '/cvtecnico', 
+  base: '/cvtecnico',
   integrations: [tailwind()],
 });
